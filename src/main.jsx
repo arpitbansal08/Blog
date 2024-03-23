d5c7d970b0a4c81a88ec58f8de4c77820c97f4c1
arpitbansal08 <101734269+arpitbansal08@users.noreply.github.com>
2024-03-23 20:27:32 +0530

    Updated basename

--- src/main.jsx
+++ src/main.jsx
@@ -1,79 +1,78 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
 import { Provider } from "react-redux";
-import store from "./store/store.js";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AddPost from "../pages/AddPost.jsx";
+import AllPosts from "../pages/AllPosts.jsx";
+import EditPost from "../pages/EditPost.jsx";
 import Home from "../pages/Home.jsx";
+import Post from "../pages/Post.jsx";
+import App from "./App.jsx";
 import Login from "./components/Login.jsx";
 import SignUp from "./components/Signup.jsx";
 import { AuthLayout } from "./components/index.js";
-import AllPosts from "../pages/AllPosts.jsx";
-import AddPost from "../pages/AddPost.jsx";
-import EditPost from "../pages/EditPost.jsx";
-import Post from "../pages/Post.jsx";
+import "./index.css";
+import store from "./store/store.js";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      { path: "/", element: <Home /> },
-      {
-        path: "/login",
-        element: (
-          <AuthLayout authentication={false}>
-            <Login />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/signup",
-        element: (
-          <AuthLayout authentication={false}>
-            <SignUp />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/all-posts",
-        element: (
-          <AuthLayout authentication>
-            {" "}
-            <AllPosts />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/add-post",
-        element: (
-          <AuthLayout authentication>
-            {" "}
-            <AddPost />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/edit-post/:slug",
-        element: (
-          <AuthLayout authentication>
-            {" "}
-            <EditPost />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/post/:slug",
-        element: (
-         
-            <Post />
-          
-        ),
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        { path: "/", element: <Home /> },
+        {
+          path: "/login",
+          element: (
+            <AuthLayout authentication={false}>
+              <Login />
+            </AuthLayout>
+          ),
+        },
+        {
+          path: "/signup",
+          element: (
+            <AuthLayout authentication={false}>
+              <SignUp />
+            </AuthLayout>
+          ),
+        },
+        {
+          path: "/all-posts",
+          element: (
+            <AuthLayout authentication>
+              {" "}
+              <AllPosts />
+            </AuthLayout>
+          ),
+        },
+        {
+          path: "/add-post",
+          element: (
+            <AuthLayout authentication>
+              {" "}
+              <AddPost />
+            </AuthLayout>
+          ),
+        },
+        {
+          path: "/edit-post/:slug",
+          element: (
+            <AuthLayout authentication>
+              {" "}
+              <EditPost />
+            </AuthLayout>
+          ),
+        },
+        {
+          path: "/post/:slug",
+          element: <Post />,
+        },
+      ],
+    },
+  ],
+  { baseName: "/Blog" }
+);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
